2d0b29980155926f1668cbad0476594ed797de21
thesmuniz <steven.muniz@gmail.com>
2020-01-10 01:09:29 -0600

    Built out the photo and photo grid components

--- learn-redux/client/reducers/post.js
+++ learn-redux/client/reducers/post.js
@@ -4,6 +4,7 @@
 // 2. Copy of the current state
 
 function posts(state = [], action) {
+  console.log('The post will change');
   console.log(state, action);
   return state;
 }
--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -5,21 +5,23 @@ import { render } from 'react-dom';
 import css from './styles/style.styl';
 
 // Import Components
-import Main from './components/Main'
+import Main from './components/App'
 import Single from './components/Single'
 import PhotoGrid from './components/PhotoGrid'
 
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { Router, Route, IndexRoute } from 'react-router';
 import { Provider } from 'react-redux';
 import store, { history } from './store';
 
 const router = (
-  <Router history={browserHistory}>
-    <Route path="/" component={Main}>
-      <IndexRoute component={PhotoGrid}></IndexRoute>
-      <Route path="/view/:postId" component={Single}></Route>
-    </Route>
-  </Router>
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={Main}>
+        <IndexRoute component={PhotoGrid}></IndexRoute>
+        <Route path="/view/:postId" component={Single}></Route>
+      </Route>
+    </Router>
+  </Provider>
 )
 
 render(router, document.getElementById('root'));
