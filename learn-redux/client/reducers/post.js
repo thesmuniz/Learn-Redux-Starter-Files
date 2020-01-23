300105b84536a1bc4ad15ccdcfe784120b5f9f19
Steven Muniz <smuniz@cylance.com>
2020-01-23 15:14:26 -0600

    Updated the posts reducer to allow for binding of data to be passed to update total likes

--- learn-redux/client/reducers/post.js
+++ learn-redux/client/reducers/post.js
@@ -5,10 +5,16 @@
 
 function posts(state = [], action) {
   switch(action.type) {
-    case 'INCREMENT_LIKES' :
-      console.log('Incrementing Likes!!');
-    default:
-      return state;
+    case 'INCREMEMENT_LIKES':
+      console.log('Incrementing Likes')
+      const i = action.index;
+      return [
+        ...state.slice(0,i), // before the one we are updating
+        {...state[i], likes: state[i].likes + 1},
+        ...state.slice(i + 1), // after the one we are updating
+      ]
+      default:
+        return state;
   }
 }
 
