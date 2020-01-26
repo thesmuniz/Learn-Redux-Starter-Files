17c0032f362f1b9a1a72efa10eb96b5c00014f1a
Steven Muniz <smuniz@cylance.com>
2020-01-26 01:33:27 -0600

    Completion of Redux, connecting to Chrome Redux tools

--- learn-redux/client/actions/actionCreators.js
+++ learn-redux/client/actions/actionCreators.js
@@ -8,6 +8,7 @@ export function increment(index) {
 
 // add comment
 export function addComment(postId, author, comment) {
+  console.log('Dispaching')
   return {
     type: 'ADD_COMMENT',
     postId,
--- learn-redux/client/reducers/comments.js
+++ learn-redux/client/reducers/comments.js
@@ -1,4 +1,34 @@
+function postComments(state = [], action) {
+  switch(action.type) {
+    case 'ADD_COMMENT':
+      // return the new state with the new comment
+      return [...state,{
+        user: action.author,
+        text: action.comment
+      }]
+    case 'REMOVE_COMMENT':
+      // We need to return the new state without the delected comment
+      return [
+        // From the start to the one we want to delete
+        ...state.slice(0, action.i),
+        // After the deleted one, to the end
+        ...state.slice(action.i + 1)
+      ]
+    default:
+      return state;
+  }
+  return state;
+}
+
 function comments(state = [], action) {
+  if(typeof action.postId !== 'undefined') {
+    return {
+      // take the current state
+      ...state,
+      // overtite this post with a new one
+      [action.postId]: postComments(state[action.postId], action)
+    }
+  }
   return state;
 }
 
